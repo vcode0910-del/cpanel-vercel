8daf0a576452eedb70781a465d106af7f60c8c0f
vcode0910-del <vcode0910@gmail.com>
2026-01-02 22:21:16 +0700

    index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 // --- PENGATURAN API ---
-$PANEL_URL = "https://panel.domainanda.com"; // Ganti dengan URL Panel Anda
-$API_KEY   = "ptla_xxxxxxxxxxxxxxxxxxxx";   // Ganti dengan API Key Application Anda
+$PANEL_URL = "https://duhaistore.servercloud.my.id/"; // Ganti dengan URL Panel Anda
+$API_KEY   = "ptla_AaTVmBOOKxi5LMlQQUh2gOMlooFXxvAw7PApfEs4iyX";   // Ganti dengan API Key Application Anda
 // ----------------------
 
 $message = "";
